b651b6c86cfb8dba61f6f43420d0834ed3fedf13
Eri Ahmed <sahmed12@pride.hofstra.email>
2018-02-09 15:36:39 -0500

    taking screenshots for assignment

--- src/cybermissile/CyberMissile.java
+++ src/cybermissile/CyberMissile.java
@@ -15,7 +15,6 @@ public class CyberMissile {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         FlightSystem fs = new FlightSystem();
         Warhead wh = new Warhead();
         fs.setTarget("The Moon");
